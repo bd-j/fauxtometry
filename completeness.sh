4cb8e390b0bdbc1415d60a6b0ef522e80c24b9e6
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2024-02-09 16:13:45 -0500

    be more talkative.

--- completeness.sh
+++ completeness.sh
@@ -1,17 +1,20 @@
 # for each realization
 
-echo tag=$tag
-
 image_dir=${base}/${pid}/${ver}_mosaic
 mock_dir=${base}/${pid}/${ver}_mock
 mkdir -p $mock_dir
 
+echo tag=$tag
+echo mock_dir=$mock_dir
+
 # generate fake images in detection bands
+echo "injecting fake sources"
 python source_injection.py --image_names $image_dir/mosaic_F200W.fits $image_dir/mosaic_F150W.fits \
                            --n_fake 6400 \
                            --tag $tag \
                            --mock_dir $mock_dir
 # run detection
+echo "detecting sources in fake images"
 python ecat_sandro.py --mosaic_dir $mock_dir \
                       --tag $tag \
                       --bands F200W F150W
@@ -19,6 +22,7 @@ python ecat_sandro.py --mosaic_dir $mock_dir \
 # compare input and output, constructing completeness catalog
     # 1 = detected
     # 0 = undetected
+echo "constructing recovery catalog."
 python completeness.py --mock_dir $mock_dir \
                        --tag $tag \
                        --band F200W
