e942dd81978336a5cd9f10f589fc5a46ae26fa23
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2024-02-21 10:04:46 -0500

    plotting.

--- plotting.py
+++ plotting.py
@@ -94,9 +94,15 @@ if __name__ == "__main__":
     # color including aperture corrections.
     incolor = -2.5*np.log10(mcat["F200W"]/mcat["F444W"])
     color = -2.5 * np.log10((rcat[f"F200W_aper1"]*1.55) / ( rcat[f"F444W_aper1"]*3.3))
+    cmd_color = -2.5 * np.log10((rcat[f"F150W_aper1"]*1.53) / ( rcat[f"F200W_aper1"]*1.55))
 
     # --- selection ---
-    weight = mcat["detected"] & np.isfinite(color) & (incolor < -1.4) & (incolor > -1.6)
+    weight = (mcat["detected"]
+              & np.isfinite(color)
+              & ((-2.5 * np.log10(rcat[f"F200W_aper1"]*1.55/3631e9)) < 30)
+              & (cmd_color < -0.00) & (cmd_color > -0.5)
+              #& (color < -1)
+            )
 
     # --- define mag and radius bins ---
     mbins = np.arange(28.0, 31.1, 0.1)
@@ -114,6 +120,7 @@ if __name__ == "__main__":
 
     N = len(sbins) - 1
     ctable = np.zeros([N, len(rbins)-1])
+    etable = np.zeros([N, len(rbins)-1])
 
     pl.rcParams["axes.prop_cycle"] = pl.cycler("color", pl.cm.viridis(np.linspace(0,1,N)))
     fig, ax = pl.subplots()
@@ -125,14 +132,17 @@ if __name__ == "__main__":
         hin, _ = np.histogram(x, bins=xbins)
         hout, _ = np.histogram(x,  weights=w, bins=xbins)
         comp = (hout/hin)
+        err = np.sqrt(comp / hin * (1 + comp))
         ctable[i] = comp
+        etable[i] = err
         xm = (xbins[:-1] + xbins[1:]) / 2
         print(sbin, comp[-9:].min(), comp[-9:].max())
-        ax.plot(xm, hout / hin, "-o", label=f"{sbins[i]:.1f} < {sname} < {sbins[i+1]:.1f}")
+        ax.errorbar(xm, comp, yerr=err, color="grey", linestyle="", marker="")
+        ax.plot(xm, comp, "-o", label=f"{sbins[i]:.1f} < {sname} < {sbins[i+1]:.1f}")
 
     ax.set_xlabel(xname)
     ax.set_ylabel("detected fraction")
-    ax.legend()
+    ax.legend(loc="upper left")
     ax.set_xscale("log")
     fig.savefig(f"{results}/bluej_completness_vs_radius.png")
 
@@ -141,7 +151,7 @@ if __name__ == "__main__":
         magbins = " ".join([f"f200w={mm:.2f}" for mm in sbm])
         fout.write(f"outer radius(')  {magbins} \n")
         for i, rbin in enumerate(rbins[1:]):
-            cc = " ".join([f"{c:.3f}" for c in ctable[:, i]])
+            cc = " ".join([f"{c:.3f} {e:.4f}" for c, e in zip(ctable[:, i], etable[:,i])])
             fout.write(f"{rbin:.2f} {cc}\n")
 
     if False:
@@ -150,27 +160,30 @@ if __name__ == "__main__":
                                  bins=[rbins, mbins])
 
     # --- F200W ----
+    min_dist_ark = 2.0
 
     #total completeness
     xx, xbins, xname = inmags, mbins, "F200W"
-    sel = radius/60. > 1.5
+    sel = radius/60. > min_dist_ark
     x = xx[sel]
     w = weight[sel]
     hin, _ = np.histogram(x, bins=xbins)
     hout, _ = np.histogram(x,  weights=w, bins=xbins)
     comp = (hout/hin)
+    err = np.sqrt(comp / hin * (1 + comp))
     xm = (xbins[:-1] + xbins[1:]) / 2
-    fig, ax = pl.subplots()
+    cfig, ax = pl.subplots()
+    ax.errorbar(xm, comp, yerr=err, color="grey", linestyle="", marker="")
     ax.plot(xm, comp, "-o")
     ax.set_xlabel(xname)
     ax.set_ylabel("selected fraction (detection + color)")
-    ax.set_title("d_ark > 1.5 arcmin")
-    fig.savefig(f"{results}/bluej_completness_vs_F200W.png")
+    ax.set_title(f"d_ark > {min_dist_ark} arcmin")
+    cfig.savefig(f"{results}/bluej_completness_vs_F200W.png")
 
     # magnitude offsets
     delta = (outmags - inmags)
     sel = (np.isfinite(outmags)) & (mcat["detected"] > 0) & (radius > (2 * 60))
-    fig, ax = pl.subplots(figsize=(8, 5.8))
+    dfig, ax = pl.subplots(figsize=(8, 5.8))
     cb = ax.hexbin(inmags[sel], delta[sel],
                    extent=(28.0, mbins.max(), 0.11, 0.9), gridsize=[40, 20])
     ax.set_xlabel(f"{sname} TOTAL_input (mag)")
@@ -189,12 +202,13 @@ if __name__ == "__main__":
 
     ax.set_ylim(0.11, 0.9)
 
-    fig.savefig(f"{results}/delta_F200W.png")
+    dfig.savefig(f"{results}/delta_F200W.png")
 
     # text table
     with open(f"{results}/bluej_completness_vs_F200W.dat", "w") as fout:
-        fout.write("total_f200w  completeness  dm50 dm16 dm84\n")
+        fout.write(f"# dist_ark > {min_dist_ark} arcmin\n")
+        fout.write("#total_f200w  completeness completeness_unc dm50 dm16 dm84\n")
         for i, m in enumerate(xm):
-            fout.write(f"{m:.1f} {comp[i]:.3f} {pct[i, 0]:.3f} {pct[i, 1]:.3f} {pct[i, 2]:.3f}\n")
+            fout.write(f"{m:.1f} {comp[i]:.3f} {err[i]:.4f} {pct[i, 0]:.3f} {pct[i, 1]:.3f} {pct[i, 2]:.3f}\n")
 
 #    ax.imshow(h2out/h2in, extents)
